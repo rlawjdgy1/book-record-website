3593c893ad3ddd84b7263904c9802d7d0611174b
김정효 <rlawjdgy1@likelion.org>
2021-02-16 13:38:33 +0900

    permissions

--- accounts/views.py
+++ accounts/views.py
@@ -4,18 +4,49 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def register(request):
-    if request.method == "POST":
-        form = RegistrationForm(request.POST or None)
-        if form.is_valid():
-            user = form.save()
-
-            raw_password = form.cleaned_data.get('password1')
-
-            user = authenticate(username=user.username, password=raw_password)
-
-            login(request, user)
-
-            return redirect("main:home")
+    if request.user.is_authenticated:
+        return redirect("main:home")
     else:
-        form = RegistrationForm()
-    return render(request, "accounts/register.html", {"form": form})
+        if request.method == "POST":
+            form = RegistrationForm(request.POST or None)
+            if form.is_valid():
+                user = form.save()
+
+                raw_password = form.cleaned_data.get('password1')
+
+                user = authenticate(username=user.username, password=raw_password)
+
+                login(request, user)
+
+                return redirect("main:home")
+        else:
+            form = RegistrationForm()
+        return render(request, "accounts/register.html", {"form": form})
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect("main:home")
+    else:
+        if request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect("main:home")
+                else:
+                    return render(request, 'accounts/login.html', {"error": "Your account has been disabled."})
+            else:
+                return render(request, 'accounts/login.html', {"error": "Invalid Username or Password. Try Again."})
+        return render(request, 'accounts/login.html')
+
+def logout_user(request):
+    if request.user.is_authenticated:
+        logout(request)
+        print("로그아웃 되었습니다")
+        return redirect("accounts:login")
+    else:
+        return redirect("accounts:login")
--- main/views.py
+++ main/views.py
@@ -22,34 +22,54 @@ def detail(request, id):
     return render(request, 'main/details.html', context)
 
 def add_books(request):
-    if request.method == "POST":
-        form = BookForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            if request.method == "POST":
+                form = BookForm(request.POST or None)
 
-        if form.is_valid():
-            data = form.save(commit=False)
-            data.save()
+                if form.is_valid():
+                    data = form.save(commit=False)
+                    data.save()
+                    return redirect("main:home")
+            else:
+                form = BookForm()
+            return render(request, 'main/addbooks.html', {"form": form, "controller": "책 추가하기"})
+        else:
             return redirect("main:home")
-    else:
-        form = BookForm()
-    return render(request, 'main/addbooks.html', {"form": form, "controller": "책 추가하기"})
+
+    return redirect("accounts:login")
 
 #edit the movie
 def edit_books(request, id):
-    book = Book.objects.get(id=id)
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+    
+            book = Book.objects.get(id=id)
 
-    if request.method == "POST":
-        form = BookForm(request.POST or None, instance=book)
+            if request.method == "POST":
+                form = BookForm(request.POST or None, instance=book)
 
-        if form.is_valid():
-            data = form.save(commit=False)
-            data.save()
-            return redirect("main:detail", id)
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'main/addbooks.html', {"form":form, "controller": "책 정보 수정하기"})
+                if form.is_valid():
+                    data = form.save(commit=False)
+                    data.save()
+                    return redirect("main:detail", id)
+            else:
+                form = BookForm(instance=book)
+            return render(request, 'main/addbooks.html', {"form":form, "controller": "책 정보 수정하기"})
+        else:
+            return redirect("main:home")
+    
+    return redirect("accounts:login")
 
 def delete_books(request, id):
-    book = Book.objects.get(id=id)
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            book = Book.objects.get(id=id)
 
-    book.delete()
-    return redirect("main:home")
+            book.delete()
+            return redirect("main:home")
+        else:
+            return redirect("main:home")
+    
+    return redirect("accounts:login")
+        
